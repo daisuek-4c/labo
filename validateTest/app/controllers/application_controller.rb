7b9bfcb5ba709767798c34546477afca56399972
daisuek4cd <d_yoshida@ap-com.co.jp>
2014-06-26 04:26:08 +0000

    session test

--- validateTest/app/controllers/application_controller.rb
+++ validateTest/app/controllers/application_controller.rb
@@ -2,4 +2,52 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :check_authentication
+
+  def check_authentication
+    logger.debug "session check before action : start"
+ 
+#    session["cloudn.rdb"] =true
+ 
+    # OPへリダイレクト 
+    redirect_to get_openid_redirect_url if session["cloudn.rdb"] == nil
+ 
+    # セッションにAPIキー情報は入れないので、都度API実行で取得
+    api_key = get_api_key
+    @api_access_key = "hogehoge"
+    @api_secret_access_key = "fugafuga"
+ 
+    logger.debug @api_access_key
+    logger.debug @api_secret_access_key
+ 
+    logger.debug "session check before action : end"
+  end
+
+  def get_openid_redirect_url
+      begin
+        # リダイレクトURLを取得
+        redirect_url = openid_request
+        logger.debug "redirect because of session is nil."
+        logger.debug redirect_url
+ 
+      rescue
+        logger.fatal "hoge"
+ 
+      else
+        return redirect_url
+ 
+      end
+  end
+
+  # ここは、OpenID用の別クラスに切り出す
+  def openid_request
+    return "http://www.google.co.jp/"
+  end
+ 
+  # ここは、キー取得用の別クラスに切り出す
+  def get_api_key
+ 
+  end
+
 end
